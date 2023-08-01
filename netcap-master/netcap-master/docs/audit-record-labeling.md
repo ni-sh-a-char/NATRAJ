60396e07ed3615b860b3926697decb31b57ccc1e
dsfksh <65788721+PIYUSH-MISHRA-00@users.noreply.github.com>
2023-08-02 04:11:15 +0530

    added notebooks

--- netcap-master/netcap-master/docs/audit-record-labeling.md
+++
@@ -1,2 +0,0 @@
-# Audit Record Labeling
-
--- netcap-master/version.go
+++
@@ -1,8 +0,0 @@
-// Package netcap is a framework for network traffic analysis. Learn more at https://netcap.io.
-package netcap
-
-// Version is the current version identifier for netcap.
-var Version = "v0.6.11"
-
-// Commit is the git commit id of the current version.
-var Commit = "39a85c6357a0dc7aeca2e869176903adef5c0ed3"
